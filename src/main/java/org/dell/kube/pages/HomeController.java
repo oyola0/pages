b56a4da5d2523bd6eb18b61a5c6878ea57d10374
josoy87@gmail.com <josoy87@gmail.com>
2021-01-12 16:17:20 +0000

    logging and monitoring

--- src/main/java/org/dell/kube/pages/HomeController.java
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.classic.Logger;
+
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
+
     private String pageContent;
 
     public HomeController(@Value("${page.content}") String pageContent){
